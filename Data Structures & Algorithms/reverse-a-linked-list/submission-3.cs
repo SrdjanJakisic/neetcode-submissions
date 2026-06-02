7b6f13c95c018ad77ef0750b5a3664d6f1a104e6
SrdjanJakisic <91378107+SrdjanJakisic@users.noreply.github.com>
2026-06-02 11:21:55 +0200

    Add: reverse-a-linked-list - submission-3

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-3.cs
@@ -0,0 +1,17 @@
+
+ 
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        if(head == null) return null;
+
+        ListNode newHead = head;
+        if(head.next != null)
+        {
+            newHead = ReverseList(head.next);
+            head.next.next = head;
+        }
+
+        head.next = null;
+        return newHead;
+    }
+}
